b6d0a9e7638fcb53360f46dd98392d09f12879d1
Thomas Getgood <thomas@macroexpanse.com>
2023-02-20 18:32:55 +0100

    This likely isn't something suitable for browser APIs.
    I need control over exactly how many frames the target is visible for. Sadly,
    the browser buries that knowledge deeply.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,27 @@
 <script>
-  let number = Math.floor(Math.random()*1000)
-  let hidden = false
+  let number = Math.floor(Math.random()*1000);
+  let hidden = true;
+  let height = 800;
+  let width = 1000;
 
-  const tac = function () {
-    hidden = false
-    requestAnimationFrame(() => hidden = true)
-  }
+  const tac = function (e) {
+    console.log(e);
+    requestAnimationFrame(() => {
+      hidden = false;
+      requestAnimationFrame(() => {
+        hidden = true
+      });
+    });
+  };
+
+  function press (e) {
+    console.log(e);
+  };
 </script>
 
 <p>Press [Spacebar] to flash next image</p>
 
-<div id=outer>
+<div id=outer style="height: {height}px; width: {width}px;" on:click={tac}>
 <h2 class={hidden ? "hide" : "show"}>{number}</h2>
 </div>
 
