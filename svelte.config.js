3c3143f72bc0e4a3584b6a1ea16a5966657cb65e
Thomas Getgood <thomas@macroexpanse.com>
2023-03-09 17:43:34 -0500

    Configured static production build.

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: dev ? '' : '/tachi'
+    }
 	}
 };
 
