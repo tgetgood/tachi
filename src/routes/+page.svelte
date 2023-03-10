261bfe4cc59c57a6c1649f1449bae30f32169f41
Thomas Getgood <thomas@macroexpanse.com>
2023-03-10 13:37:11 -0500

    Progress towards the answering element I want.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <script>
+  const init = Symbol("init");
+  const blink = Symbol("blink");
+  const input = Symbol("input");
+  const next = Symbol("next");
+  const again = Symbol("again");
+
+  let state = [init, 0];
+
   let number = "";
   let hidden = true;
   let height;
@@ -10,19 +18,15 @@
   // Difficulty level (number of digits to display)
   let level = 4;
 
-  let guess = "";
+  let guess = [];
 
-  $: tg = guess.trim();
-
-  $: correctD = correctDigits(tg, number);
+  $: correctD = correctDigits(guess, number);
   $: correctP = correctD == level;
 
 
   function correctDigits(a, b) {
-    console.log(a, b)
     let out = 0;
     for (let i = 0; i < Math.min(a.length, b.length); i++) {
-      console.log(i, a[i], b[i])
       if (a[i] == b[i]) {
         out += 1;
       }
@@ -44,28 +48,39 @@
     setsize(this);
   }
 
+
+  function resetGame() {
+
+  }
+
+  function adjustLevel(result) {
+  }
+
   function flicker (n) {
-    hidden = false;
-    if (n > 0) {
-      requestAnimationFrame(() => flicker(n - 1));
-    } else {
-      hidden = true;
-    }
+    state = [init, 0];
+    setTimeout(Math.random()*1000+500, (() => {
+      state = [blink, 0];
+      if (n > 0) {
+        requestAnimationFrame(() => flicker(n - 1));
+      } else {
+        state = [input, 0];
+        focus(state);
+      }
+    })());
   }
 
   function tac (e) {
 
     if (e.key === ' ') {
-      console.log(correctDigits(guess.trim(), number))
 
       // Try again until correct
-      if (number == "" || correctP) {
+      // if (number == "" || correctP) {
         reset(level);
-      };
+      // };
 
-      guess = '';
+      guess = ["", "", "", ""];
       flicker(4);
-      document.getElementById("guess").focus();
+
     }
   }
 
@@ -82,28 +97,67 @@
     } else {
       number = n.toString();
     }
-  };
+  }
+
+  function show(state, s) {
+    if (state[0] == s) {
+      return 'display: block;';
+    } else {
+      return 'display: none;';
+    }
+  }
+
+  function focus(state) {
+    if (state[0] == input) {
+      const el = document.getElementById("digit-"+state[1]);
+      setTimeout(10, el.focus());
+    }
+  }
+
+  function answerEntry(e) {
+    e.stopPropagation();
+    if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].indexOf(e.key) >= 0) {
+      e.preventDefault();
+      guess[state[1]] = e.key;
+      if (state[1] < level - 1) {
+        state = [state[0], state[1] + 1]
+        focus(state)
+      } else {
+        state = evaluateAnswer(guess);
+      } 
+    } else if (e.key == 'Backspace' && state[1] > 0) {
+      state = [state[0], state[1] - 1];
+      focus(state);
+    } else {
+      e.preventDefault();
+    }
+  }
+  
 </script>
 
 <svelte:window on:keypress={tac} use:onload on:resize={onresize}/>
 
-
 <div id=outer style="top: {height/2}px; left: {width/2}px;">
-  <h2 id="inner">{hidden ? "" : number}</h2>
+  <h2 class="blink" style={show(state, blink)}>{number}</h2>
 
-  <input id="guess" type=none size=5 bind:value={guess} class={correctP ? "green" : ""}>
+  <div style={show(state, input)} id="answer-container">
+    {#each guess as digit, index}
+      <input id="digit-{index}"
+             type=none
+             value={digit}
+             class={(correctP ? "green" : "") + "answer-digit"}
+             on:keydown={answerEntry}
+             >
+    {/each}
+  </div>
 
-  <p>Press [Spacebar] to flash next image</p>
+  <p style={show(state, next)}>Press [Spacebar] to flash next image</p>
 </div>
 
 
 
 <style>
-  .hide {
-    display: none;
-  }
-
-  div#outer {
+  #outer {
     position: absolute;
   }
 
@@ -111,11 +165,21 @@
     height: 2em;
   }
 
-  #guess {
-    border-radius: 0.7em;
-    border-width: 4px;
-    padding: 0.2em;
-    border: solid;
+  #answer-container {
+    display: flex;
+  }
+
+  .answer-digit {
+    height: 1.4em;
+    width: 0.6em;
+    margin-left: 0.1em;
+    padding-left: 0.1em;
+    border-radius: 0.3em;
+    border-width: 0.1em;
+    border-color: lightgrey;
+
+
+    font-size: 1.4em;
   }
 
   .green {
